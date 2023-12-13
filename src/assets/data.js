6f21295885d70551cdc74784a3eed3a4875579dc
rashid1709 <rashid.ali2009@gmail.com>
2023-12-14 00:10:33 +0530

    first commit

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        question:"which device is required for the internet connection?",
+        option1:"Modem",
+        option2:"Router",
+        option3:"LAN Cable",
+        option4:"Pen Drive",
+        ans:1,
+    },
+    {
+        question:"which continent has the highest number of country?",
+        option1:"Asia",
+        option2:"Europe",
+        option3:"North America",
+        option4:"Africa",
+        ans:4,
+    },
+    {
+        question:"Junk email is also called?",
+        option1:"Spam",
+        option2:"Fake",
+        option3:"Archived",
+        option4:"Bin",
+        ans:1,
+    },
+    {
+        question:"A computer cannot boot if it does not have the?",
+        option1:"Application Software",
+        option2:"Internet",
+        option3:"Operating System",
+        option4:"Mouse",
+        ans:3,
+    },
+    {
+        question:"First page of website is termed as?",
+        option1:"Index Page",
+        option2:"Home Page",
+        option3:"Site map",
+        option4:"Landing Page",
+        ans:2,
+    },
+]
